8672fc0a1d39f5d078e5834898a867152f4a8d29
mvp-man <manuelvp05@hotmail.com>
2021-02-09 17:05:36 -0500

    1st commit

---
+++ src/Ventura.SAP.Tools.CrossCutting/Utils/SAPHelper.cs
@@ -0,0 +1,18 @@
+﻿using SAPbobsCOM;
+
+namespace Ventura.SAP.Tools.UDO.Utils
+{
+    public class SAPHelper
+    {
+        public static Company GetCompany(string companyName)
+        {
+            var company = new Company();
+            company.Server = "LAPTOP-4BKA2N4K";
+            company.DbServerType = BoDataServerTypes.dst_MSSQL2014;
+            company.CompanyDB = companyName;
+            company.UserName = "manager";
+            company.Password = "1234";            
+            return company;
+        }
+    }
+}
---
+++ src/Ventura.SAP.Tools.UDO/Remove.cs
@@ -0,0 +1,42 @@
+﻿using SAPbobsCOM;
+using System;
+using Ventura.SAP.Tools.UDO.Utils;
+
+namespace Ventura.SAP.Tools.UDO
+{
+    public class Remove
+    {
+        private static int ERROR_CODE;
+        private static string ERROR_MESSAGE;
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var company = SAPHelper.GetCompany("SBO_POLYARQ_PROD");
+            if (company.Connected)
+                company.Disconnect();
+            var result = company.Connect();
+            if (result != 0)
+            {
+                company.GetLastError(out ERROR_CODE, out ERROR_MESSAGE);
+                Console.Error.Write(string.Format("({0})-{1}", ERROR_CODE, ERROR_MESSAGE));
+                Console.Read();
+                Environment.Exit(ERROR_CODE);
+            }
+
+            var udoManager = (UserObjectsMD)company.GetBusinessObject(BoObjectTypes.oUserObjectsMD);
+            for (int i = 0; i < args.Length; i++)
+            {
+                var item = args[i];
+                if (!udoManager.GetByKey(item))
+                {
+                    Console.WriteLine("Don't exists the UDO {0}", item);
+                    continue;
+                }
+                udoManager.Remove();
+            }
+            Console.Read();
+            company.Disconnect();
+        }
+    }
+}
