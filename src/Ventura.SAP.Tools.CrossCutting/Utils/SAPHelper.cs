b9d820086b1ac6051c82673faf2e8af509d4c8d0
mvp-man <manuelvp05@hotmail.com>
2021-03-30 16:25:48 -0500

    feat: hana conexion

--- src/Ventura.SAP.Tools.CrossCutting/Utils/SAPHelper.cs
+++ src/Ventura.SAP.Tools.CrossCutting/Utils/SAPHelper.cs
@@ -4,14 +4,24 @@ namespace Ventura.SAP.Tools.CrossCutting.Utils
 {
     public static class SAPHelper
     {
+        //public static Company GetCompany()
+        //{
+        //    var company = new Company();
+        //    company.Server = "LAPTOP-4BKA2N4K";
+        //    company.DbServerType = BoDataServerTypes.dst_MSSQL2014;
+        //    company.CompanyDB = "SBO_POLYARQ_PROD";
+        //    company.UserName = "manager";
+        //    company.Password = "1234";            
+        //    return company;
+        //}
         public static Company GetCompany()
         {
             var company = new Company();
-            company.Server = "LAPTOP-4BKA2N4K";
-            company.DbServerType = BoDataServerTypes.dst_MSSQL2014;
-            company.CompanyDB = "SBO_POLYARQ_PROD";
+            company.Server = "192.168.1.14:30015";
+            company.DbServerType = BoDataServerTypes.dst_HANADB;
+            company.CompanyDB = "SBO_TEST_DEPO";
             company.UserName = "manager";
-            company.Password = "1234";            
+            company.Password = "sbosap";
             return company;
         }
     }
